db6ef77ce2130137cb2e70110210e23f875433cd
Josephinex00 <josephineodus@gmail.com>
2022-08-07 21:43:39 +0100

    uploading more chapter 2 questions

---
+++ chapter2/q2.py
@@ -0,0 +1,5 @@
+#2
+#print vertically???
+i = 0
+for i in range (0, 100):
+    print('Josephine', end='')
---
+++ chapter2/q3.py
@@ -0,0 +1,5 @@
+#3
+i = 0
+for i in range (0, 100):
+    i = i+1
+    print(i, 'Josephine')
